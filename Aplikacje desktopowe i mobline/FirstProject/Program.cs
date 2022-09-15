c95b8cd40f4b2f7252988938fd6df6604fe46475
gutataw <skfafka>
2022-09-15 09:06:01 +0200

    lekcja 15.09.2022

--- Aplikacje desktopowe i mobline/FirstProject/Program.cs
+++ Aplikacje desktopowe i mobline/FirstProject/Program.cs
@@ -6,8 +6,19 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            string imie = "jan";
+            string nazwisko= "kowalski";
+            //nazwisko.Length
+            Console.WriteLine("witaj"+nazwisko+" "+imie+"tutaj");
+            Console.WriteLine("witaj {0},{1} tutaj",imie,nazwisko);
+            Console.WriteLine($"witaj {imie},{nazwisko} tutaj");
+
+            Console.WriteLine("Prętkość to km\\h");//zanak "\ " jest specjany  i program nie działa jak użyje się 2 razy \ to zadziała
+            Console.WriteLine(@"Prętkość to km\h");//można zamiast 2 razy \ można wstawić @ przed tekstem 
+
+            int liczba=5;
+
+
         }
     }
 }
