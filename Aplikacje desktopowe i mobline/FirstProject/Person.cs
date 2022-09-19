112d5fa0686eb1614f4d794838375d98852495f6
gutataw <skfafka>
2022-09-19 16:03:42 +0200

    koniec lekcji (nie dokonczone)

---
+++ Aplikacje desktopowe i mobline/FirstProject/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class FirstPerson
+    {
+        // ctrl k d układa ładnie kod
+        private string person;
+        private string LastName;
+        private int age;
+
+        public FirstPerson() // ctor i 2 razy tab aby zorbic konstruktor 
+        {
+            person = "adam";// crtl r r wtedy zamieniasz wszyskie zaznaczone nazy 
+            LastName = "Michta";
+            age = 30;
+
+        }
+        public FirstPerson(string i, string n, int w)
+        {
+            i = person;
+            n = LastName;
+            w = age;
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("********************************************");
+            Console.WriteLine($"Imie:{name}");
+            Console.WriteLine($"nazwisko:{LastName}");
+            Console.WriteLine($"wiek:{age}");
+            Console.WriteLine("********************************************");
+
+        }
+
+    }
+}
--- Aplikacje desktopowe i mobline/FirstProject/Program.cs
+++ Aplikacje desktopowe i mobline/FirstProject/Program.cs
@@ -18,6 +18,11 @@ namespace FirstProject
 
             int LiczbaCalkowita=5;
             double LiczbaRzeczywista=6.6;
+
+            Person FirstPerson = new Person;
+            Program.ShowInfo();
+
+            
                         
         }
     }
