b04bb024e3404c2b626922ce601af4aed3a5e7f3
gutataw <skfafka>
2022-09-29 07:33:50 +0200

    ee

---
+++ Aplikacje desktopowe i mobline/passing_parameters_to_methods/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passing_parameters_to_methods
+{
+
+    class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+
+        }
+        public person()
+        {
+            name = "Adam";
+            surname = "Nowak";
+            age = 30;
+        }
+        public person(string i, string n, int w)
+        {
+            name = i;
+            surname = n;
+            age = w;
+        }
+        public void showInfo()
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine($"Imię: {name}");
+            Console.WriteLine($"Nazwisko: {surname}");
+            Console.WriteLine($"Wiek: {age}");
+            Console.WriteLine("*******************************");
+        }
+        public void setSurname(String newSurname)
+        {
+            if (newSurname != "")
+            {
+                surname = newSurname;
+            }
+            else
+            {
+                throw new Exception("Nazwisko zakazane");
+            }
+        }
+        public string getSurname()
+        {
+            return surname;
+        }
+
+    }
+
+
+
+    }
---
+++ Aplikacje desktopowe i mobline/passing_parameters_to_methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace passing_parameters_to_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello "); 
+        }
+    }
+}
