a794d9a7e8b9e4013175f74e0de0d4c5df584b33
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-20 13:17:34 +0200

    cvb

--- Aplikacje desktopowe i mobline/PolymorphismApp/Program.cs
+++ Aplikacje desktopowe i mobline/PolymorphismApp/Program.cs
@@ -6,7 +6,12 @@ namespace PolymorphismApp
     {
         static void Main(string[] args)
         {
-           
+            Dog firstDog = new Dog();
+            Dog secondDog = new Dog("burek", 5);
+
+
+            Dog thieDog = new Dog(secondDog);
+            
         }
     }
 }
---
+++ Aplikacje desktopowe i mobline/PolymorphismApp/dog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismApp
+{
+    class Dog
+    {
+        private string name;
+        private byte age ;
+            public Dog()
+        {
+            name = "azor";
+            age = 10;
+        }
+        public Dog(string name, byte age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Dog(Dog dogToCopy)
+        {
+            name = dogToCopy.name;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("*********************************"); 
+            Console.WriteLine("pies o imieniu " + name + "ma " + age);
+            Console.WriteLine("*********************************");
+        }
+        public void Bark()
+        {
+            
+            Console.WriteLine("pies o imieniu" + name + "szczeka");
+          
+        }
+        public void ChasePostman()
+        {
+            Console.WriteLine("pies o imi9eniu " + name + "goni listonosza");
+        }
+    }
+
+}
