5b975c11e0980144fde0e569a0d8a2417532c5f0
karol.szkola <karol.szkola@ezse.pl>
2022-12-01 13:18:19 +0100

    Lekcja tablice ( array )

--- Aplikacje desktopowe i mobline/MoveOnBoardGame/AvailableField.cs
+++ Aplikacje desktopowe i mobline/MoveOnBoardGame/AvailableField.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoveOnBoardGame.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,6 @@ namespace MoveOnBoardGame
     {
         public int X { get; set; }
         public int Y { get; set; }
-
-        public enum TypeOfObstacle TypeOfObstacle { get; set; }
+        public TypeOfObstacle TypeOfObstacle { get; set; }
     }
 }
